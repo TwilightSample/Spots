5e4c07acebdf2a8b0d84add0d3e0f066f5ad9274
Sivasankar <minimaci73@TwilightSoftwares-Mac-mini.local>
2014-07-17 16:35:19 +0530

    Initial commit
    New view controller for registration

---
+++ Spots/Spots/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  Spots
+//
+//  Created by TwilightSoftwares on 17/07/14.
+//  Copyright (c) 2014 Thiruvengadam Krishnasamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
