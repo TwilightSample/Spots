f5b0dc4023af8b90376adef159e7f8afafc106ed
Sivasankar <minimaci73@TwilightSoftwares-Mac-mini.local>
2014-07-18 15:41:10 +0530

    Login view Full
    Along with controllers with images

--- Spots/Spots/ViewController.h
+++ Spots/Spots/ViewController.h
@@ -7,14 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#include <Parse/Parse.h>
 
-@interface ViewController : UIViewController
+
+@interface ViewController : UIViewController<UITextFieldDelegate>
+
+@property (retain, nonatomic) IBOutlet UIImageView *logoImage;
+@property (weak, nonatomic) IBOutlet UIButton *forgotPassword;
+- (IBAction)forgotPassword:(id)sender;
+
 @property (weak, nonatomic) IBOutlet UITextField *usernameTxt;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTxt;
 - (IBAction)loginBtn:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *loginBtn;
-@property (weak, nonatomic) IBOutlet UIButton *cancelBtn;
-- (IBAction)cancelBtn:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *signupBtn;
 - (IBAction)signupBtn:(id)sender;
 
